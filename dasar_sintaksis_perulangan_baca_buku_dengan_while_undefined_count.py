618013103bff07d441ebc30071b873cb068ce245
Alyaaashaf <shafwa.alya@gmail.com>
2025-02-20 22:59:37 +0700

    rename variable in english

--- dasar_sintaksis_perulangan_baca_buku_dengan_while_undefined_count.py
+++ dasar_sintaksis_perulangan_baca_buku_dengan_while_undefined_count.py
@@ -1,22 +1,22 @@
 """
 Program perulangan membaca buku dengan while sampai paham
 """
-jumlah_buku = 10
+book_count = 10
 print('Ibu berkata, "Baca semua bukumu"')
-jumlah_baca = 0
+read_count = 0
 
-jumlah_paham = 0
+understood_count = 0
 print(f"Jumlah buku yang sudah dibaca dan dipahami {jumlah_paham}")
 
-while jumlah_baca < jumlah_buku * 2:
-    jumlah_baca = jumlah_baca + 1
+while read_count < book_count * 2:
+    read_count = read_count + 1
     if jumlah_paham == 9:
         print(f"Buku ke {jumlah_paham + 1} belum paham")
     else:
         jumlah_paham = jumlah_paham + 1
         print(f"Buku ke {jumlah_paham} sudah dibaca dan dipahami")
 
-if jumlah_paham == jumlah_buku:
+if jumlah_paham == book_count:
     print('"Bu, semua buku sudah dibaca dan dipahami"')
 else:
     print(f'"Bu, tidak semua buku bisa dipahami. '
